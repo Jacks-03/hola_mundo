7565ddfe78d4f1d3f1b8f11cdd9c30bbff39265a
Jaqueline Juarez Silva <jaquelinejuarez.silva03@gmail.com>
2022-03-03 17:13:57 -0600

    hola mundo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Saludo from "./Saludo"
+
+const App= () => {
+  return (
+    <div className="App">
+      <Saludo />
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Saludo.jsx
@@ -0,0 +1,4 @@
+const Saludo= ()=>{
+    return <h1>mi segundo hola mundo</h1>
+};
+export default Saludo;
